2483f012a0ae9e28045cdaca9a53b04869b2d596
JamesSantiagoDoc <109634168+JamesSantiagoDoc@users.noreply.github.com>
2022-09-15 15:15:08 +1200

    yes

--- src/api.js
+++ src/api.js
@@ -1,27 +1,64 @@
 const express = require("express")
+const cors = require('cors')
 const serverless = require("serverless-http")
-
+const { collection, ObjectId } = require("../config/database")
 const app = express()
 const router = express.Router()
 
-router.get("/", (req, res) =>{
-    res.send("User List")
+const bodyParser = require('body-parser');
+app.use(cors())
+app.use(bodyParser.urlencoded({
+
+    extended: true
+
+}));
+
+app.use(bodyParser.json());
+
+
+router.get("/", (req, res) => {
+    // res.send("User List")
+    collection.find().toArray((err, result) =>{
+        if(err) throw err;
+        res.json(result);
+    })
 })
 
-router.post("/", (req, res) =>{
-    res.send("Create User")
+router.post("/", (req, res) => {
+    collection.insertOne(req.body, (err, res) => {
+        if (err) throw err;  
+    });
+    res.send("1 document inserted");
 })
 
 router
     .route("/:id")
-    .get((req, res) =>{
-        res.send(`Get user with ID: ${req.params.id}`)
+    .get((req, res) => {    
+        //res.send(`Get User with ID ${req.params.id}`)
+        
+        collection.findOne({_id: ObjectId(req.params.id)}, (err, result) => {
+            if (err) throw err;
+            res.send(result)
+        });       
     })
-    .put()((req, res) =>{
-        res.send(`Update user with ID: ${req.params.id}`)
+    .put((req, res) => {    
+        //res.send(`Update User with ID ${req.params.id}`)
+        
+        let myquery = {_id: ObjectId(req.params.id)};
+        let newvalues = { $set: req.body };
+        collection.updateOne(myquery, newvalues, (err, result) => {
+            if (err) throw err;
+        });
+        res.send("1 document updated");
     })
-    .delete()((req, res) =>{
-        res.send(`Delete user with ID: ${req.params.id}`)
+    .delete((req, res) => {    
+        //res.send(`Update User with ID ${req.params.id}`)
+        
+        let myquery = {_id: ObjectId(req.params.id)};
+        collection.deleteOne(myquery, function(err, result) {
+            if (err) throw err;
+        });
+        res.send("1 document deleted");
     })
 
 
